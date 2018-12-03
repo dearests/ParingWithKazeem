e2451b82226cbbdced5f6c84098624c871fefffa
kazeem08 <kazeem0825@gmail.com>
2018-12-03 10:57:05 +0100

    solution

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 function leapYearCheaker(year) {
+    for(let key of year){
+        if(key % 4 === 0){
+            if(key % 100 === 0){
+                if(key % 400 === 0){
+                    console.log(key + `: it's a leap year`);
 
+
+                } else{
+                    console.log(key + `: it's not a leap year`);
+
+                }
+            }else {
+                console.log(key + `: it's a leap year`);
+                
+            }
+
+        }
+        else{
+
+            console.log(key + `: it's not a leap year`);
+
+        }
+        
+    }
 }
 
 leapYearCheaker([1996,1998,2000,2003,2004,2006,2008,2010,2012,2014,2016])
